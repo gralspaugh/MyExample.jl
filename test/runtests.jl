4d02c3f4543c120853747e6a06da51c75d87ba88
Greg Alspaugh <gregalspaugh10@gmail.com>
2021-08-08 12:16:26 -0400

    add forwardDiff dependency and safetestsets

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,10 +1,2 @@
-using MyExample
-using Test
-
-# 2x + 3y
-
-@testset "MyExample.jl" begin
-    @test my_f(2,1) == 7
-    @test my_f(2,3) == 13
-    @test my_f(1,3) == 11
-end
+using SafeTestsets
+@safetestset "My f tests" begin include("my_f_tests.jl") end
